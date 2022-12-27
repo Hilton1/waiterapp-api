9d91be2e8bddfaae635ea67be36420107eaf1730
Hilton1 <josehilton02@gmail.com>
2022-12-27 14:36:46 -0300

    Implementando listProductsByCategory

--- src/router.ts
+++ src/router.ts
@@ -7,13 +7,14 @@ import { listCategories } from './app/useCases/categories/listCategories';
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listProducts } from './app/useCases/products/listProducts';
 import { createProduct } from './app/useCases/products/createProduct';
+import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
 
 export const router = Router();
 
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, callback) {
-      callback(null, path.resolve(__dirname, '../uploads'));
+      callback(null, path.resolve(__dirname, '..', 'uploads'));
     },
     filename(req, file, callback) {
       callback(null, `${Date.now()}-${file.originalname}`);
@@ -34,9 +35,7 @@ router.get('/products', listProducts);
 router.post('/products', upload.single('image'), createProduct);
 
 // Get products by category
-router.get('/categories/:categoryId/products', (req, res) => {
-  res.send('ok!');
-});
+router.get('/categories/:categoryId/products', listProductsByCategory);
 
 // List orders
 router.get('/orders', (req, res) => {
